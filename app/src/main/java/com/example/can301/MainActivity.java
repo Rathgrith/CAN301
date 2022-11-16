b450d56dc12734295b9f5a3ef4b273e92624525a
”Rathgrithgit config --global user.email 505029658@qq.comssh-keygen -t rsa -C 505029658@qq.comgit config --global user.name ”Rathgrith <505029658@qq.com>
2022-11-16 16:10:21 +0800

    seat status manager done

--- app/src/main/java/com/example/can301/MainActivity.java
+++ app/src/main/java/com/example/can301/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.can301;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,12 +11,36 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton profileBtn;
+    private ImageView [] seatList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seatview);
+        seatList = new ImageView[16];
+        findSeat();
+        clearSeat();
+        // System.out.println("SeatList" + seatList.toString());
         profileBtn = findViewById(R.id.floatingActionButton);
         profileBtn.setOnClickListener(this::onClick);
+    }
+
+    private void findSeat(){
+        for (int i = 1; i < 17; i++) {
+            String seatID = "seat" + i;
+            // System.out.println(seatID);
+            int resID = getResources().getIdentifier(seatID, "id", getPackageName());
+            // System.out.println(resID);
+            ImageView seat = (ImageView) findViewById(resID);
+            // System.out.println(seat.toString());
+            seatList[i-1] = seat;
+        }
+    }
+
+    private void clearSeat(){
+        for (ImageView seat:
+             seatList) {
+            seat.setImageDrawable(getResources().getDrawable(R.drawable.greenseat));
+        }
     }
 
     private void onClick(View view){
