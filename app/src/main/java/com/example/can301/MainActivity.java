badbc8c4103b7a99275d3e53a86fde43334f2fe8
”Rathgrithgit config --global user.email 505029658@qq.comssh-keygen -t rsa -C 505029658@qq.comgit config --global user.name ”Rathgrith <505029658@qq.com>
2022-11-16 17:20:05 +0800

    seat connect

--- app/src/main/java/com/example/can301/MainActivity.java
+++ app/src/main/java/com/example/can301/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.can301;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.can301.net.NetAgent;
+import com.example.can301.net.OkHttpUtils;
+import com.example.can301.utilities.FastJsonUtils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton profileBtn;
@@ -22,6 +30,34 @@ public class MainActivity extends AppCompatActivity {
         // System.out.println("SeatList" + seatList.toString());
         profileBtn = findViewById(R.id.floatingActionButton);
         profileBtn.setOnClickListener(this::onClick);
+    }
+
+
+    private void getSeatStatus(){
+        HashMap hashMap = new HashMap();
+        OkHttpUtils.getSoleInstance().doPostForm("http://127.0.0.1:4523/m1/1900048-0-default/seat/listseat", new NetAgent() {
+            @Override
+            public void onSuccess(String result) {
+                Map<String,String> map =  FastJsonUtils.stringToCollect(result);
+                boolean isSuccess = Boolean.parseBoolean(map.get("isSuccess"));
+                String message = map.get("message");
+                if(isSuccess){
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast toastCenter = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+                    toastCenter.setGravity(Gravity.CENTER,0,0);
+                    toastCenter.show();
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                e.printStackTrace();
+                Toast center = Toast.makeText(getApplicationContext(), "network failure", Toast.LENGTH_SHORT);
+                center.setGravity(Gravity.CENTER,0,0);
+                center.show();
+            }
+        },hashMap,this);
     }
 
     private void findSeat(){
