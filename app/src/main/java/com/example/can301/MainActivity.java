a9ac5bea3c0849762c7aee893bb6c4ed93d086b9
zpy <9363572+Stellarise@user.noreply.gitee.com>
2022-11-18 16:57:41 +0800

    mainLayout and jump to table

--- app/src/main/java/com/example/can301/MainActivity.java
+++ app/src/main/java/com/example/can301/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.can301;
 
-import static com.example.can301.utilities.ValidateUtil.validate;
-
 import android.annotation.SuppressLint;
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -19,85 +19,64 @@ import com.example.can301.net.OkHttpUtils;
 import com.example.can301.utilities.FastJsonUtils;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import org.json.JSONObject;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    // 先写死椅子数量 16
+    private int seatNumber;
     private FloatingActionButton profileBtn;
     private FloatingActionButton flushbtn;
+    private ImageView tableBtn;
     private ImageView [] seatList;
     private int[] seatStatus;
+    //private WebView mWebview;
+    //private String weatherurl = "file:///android_asset/www/weather.html";
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // 写死椅子数 16
+        seatNumber = 16;
+        // 隐藏标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 隐藏状态栏
+        // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_seatview);
-        seatList = new ImageView[16];
-
+        seatList = new ImageView[seatNumber];
+        // mWebview = findViewById(R.id.weather);
+        tableBtn = findViewById(R.id.table1);
         flushbtn = findViewById(R.id.floatingActionButton2);
 
+        //loadWeather(weatherurl);
         findSeat();
         getSeatStatus();
-        //clearSeat();
+        // clearSeat();
         // System.out.println("SeatList" + seatList.toString());
 
         profileBtn = findViewById(R.id.floatingActionButton);
         profileBtn.setOnClickListener(this::onClick);
         flushbtn.setOnClickListener(this::flush);
-
-    }
-    public void flush(View view){
-        Intent intent = null;
-        intent = new Intent(MainActivity.this, MainActivity.class);
-        startActivity(intent);
-        clearSeat();
+        tableBtn.setOnClickListener(this::jumpToTable1);
     }
 
-    private void getSeatStatus1(){
-        HashMap hashMap = new HashMap();
-        try {
-            OkHttpUtils.getSoleInstance().doPostForm("http://10.0.2.2:8081/seat/listseat", new NetAgent() {
-                @Override
-                public void onSuccess(String result) {
-                    Map<String, String> map = FastJsonUtils.stringToCollect(result);
-                    String status = map.get("status");
-                    String message = map.get("status");
-                    String a = map.get("seatstatus");
-                    Object[] b = FastJsonUtils.toArray(a);
-                    int[] ss = new int[b.length];
-                    for (int i = 0; i < ss.length; i++) {
-                        ss[i] = Integer.parseInt(b[i].toString());
-                    }
-                    seatStatus = ss;
-                    if (status.equals("200")) {
-                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+   /* private void loadWeather(String url){
+        mWebview.getSettings().setJavaScriptEnabled(true);
+        mWebview.getSettings().setAllowFileAccess(true);
+        mWebview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebview.getSettings().setDomStorageEnabled(true);
+        mWebview.getSettings().setDefaultTextEncodingName("utf-8");
+        mWebview.loadUrl(url);
+    }*/
 
-                    } else {
-                        Toast toastCenter = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
-                        toastCenter.setGravity(Gravity.CENTER, 0, 0);
-                        toastCenter.show();
-                    }
-
-
-                }
-
-                @Override
-                public void onError(Exception e) {
-                    e.printStackTrace();
-                    Toast center = Toast.makeText(getApplicationContext(), "network failure", Toast.LENGTH_SHORT);
-                    center.setGravity(Gravity.CENTER, 0, 0);
-                    center.show();
-                }
-            }, hashMap, this);
-        }
-        catch (Exception e){
-            System.out.println(e);
-        }
+    private void flush(View view){
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
     }
+
     private void getSeatStatus(){
         HashMap hashMap = new HashMap();
         OkHttpUtils.getSoleInstance().doPostForm("http://10.0.2.2:8081/seat/listseat", new NetAgent() {
@@ -126,10 +105,10 @@ public class MainActivity extends AppCompatActivity {
                 int i=0;
                 for (ImageView seat: seatList) {
                     if(seatStatus[i]==0){
-                        seat.setImageDrawable(getResources().getDrawable(R.drawable.greenseat));
+                        seat.setImageDrawable(getResources().getDrawable(R.drawable.redseat));
                     }
                     if(seatStatus[i]==1){
-                        seat.setImageDrawable(getResources().getDrawable(R.drawable.redseat));
+                        seat.setImageDrawable(getResources().getDrawable(R.drawable.greenseat));
                     }
                     i++;
 
@@ -161,21 +140,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void clearSeat(){
+/*    private void clearSeat(){
         int i=0;
         for (ImageView seat: seatList) {
             if(seatStatus[i]==0){
-                seat.setImageDrawable(getResources().getDrawable(R.drawable.greenseat));
+                seat.setImageDrawable(getResources().getDrawable(R.drawable.grayseat));
             }
             if(seatStatus[i]==1){
-                seat.setImageDrawable(getResources().getDrawable(R.drawable.redseat));
+                seat.setImageDrawable(getResources().getDrawable(R.drawable.grayseat));
             }
-            seat.setImageDrawable(getResources().getDrawable(R.drawable.redseat));
+            seat.setImageDrawable(getResources().getDrawable(R.drawable.grayseat));
             i++;
-
-
         }
-    }
+    }*/
 
 
     private void onClick(View view){
@@ -185,6 +162,11 @@ public class MainActivity extends AppCompatActivity {
     private void jumpToProfile(){
         Intent intent = null;
         intent = new Intent(MainActivity.this, ProfileActivity.class);
+        startActivity(intent);
+    }
+    private void jumpToTable1(View view){
+        Intent intent = null;
+        intent = new Intent(MainActivity.this, TableActivity.class);
         startActivity(intent);
     }
 
