c74012ba98bcce87f5834cc5b72806021e742a92
“JeongyeongP” <pjeongyeong@gmail.com>
2022-11-20 15:57:02 +0800

    1. Update Item Exchange page design (matched with Main page(Seat View page)) 2. Add AlertDialog for Purchase Confirmation (However, not done yet. Just push it for saving Draft)

--- app/src/main/java/com/example/can301/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/can301/RecyclerViewAdapter.java
@@ -1,8 +1,11 @@
 package com.example.can301;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,6 +30,31 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         ((ViewHolder)holder).onBind(listData.get(position));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setTitle("Please Confirm");
+                builder.setMessage("You want to purchase this item?");
+
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //"YES" Button Click
+                        Toast.makeText(view.getContext(), "YES Button Click", Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //"NO" Button Click
+                        Toast.makeText(view.getContext(), "NO Button Click", Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 
     @Override
