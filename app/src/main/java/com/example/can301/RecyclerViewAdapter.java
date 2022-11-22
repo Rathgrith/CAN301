6036d3e10a7a31389db020de9e44a12ec73be6c9
“JeongyeongP” <pjeongyeong@gmail.com>
2022-11-23 01:18:36 +0800

    Delete Unnecessary activity

--- app/src/main/java/com/example/can301/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/can301/RecyclerViewAdapter.java
@@ -1,7 +1,9 @@
 package com.example.can301;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +21,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
     // list for adapter
     private ArrayList<DataItem> listData = new ArrayList<>();
+    int selectedPrice;
 
     @NonNull
     @Override
@@ -28,7 +31,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         ((ViewHolder)holder).onBind(listData.get(position));
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +45,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
                     public void onClick(DialogInterface dialog, int which) {
                         //"YES" Button Click
                         Toast.makeText(view.getContext(), "YES Button Click", Toast.LENGTH_LONG).show();
+                        selectedPrice = listData.get(position).getPrice();
+//                        System.out.println("------------Selected Price------------");
+//                        System.out.println(selectedPrice);
+                        //Save Selected Price in SharedPreferences Im not so sure:(
+                        SharedPreferences mypref = view.getContext().getSharedPreferences("price", view.getContext().MODE_PRIVATE);
+                        SharedPreferences.Editor editor = mypref.edit();
+                        editor.putString("selectedPrice", String.valueOf(selectedPrice));
+                        editor.apply();
                     }
                 });
                 builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
